f66697347240f721cab665cc2364f31df71b4fc6
Cesar Sosa <aaronash@live.com>
2024-11-28 22:05:13 -0600

    Install lint stagged to format code b4 commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { registerAllModules } from 'handsontable/registry';
-import App from './App.jsx'
-import 'handsontable/dist/handsontable.full.min.css';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { registerAllModules } from "handsontable/registry"
+import App from "./App.jsx"
+import "handsontable/dist/handsontable.full.min.css"
 
-registerAllModules();
+registerAllModules()
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
